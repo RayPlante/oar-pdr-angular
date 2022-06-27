2a19ceec69106543cf21a2d0eab3e2329632097f
Ray Plante <raymond.plante@nist.gov>
2022-06-27 07:54:10 -0400

    remove references to archwizard

--- midas-author/src/app/startwiz/stepwizard.module.ts
+++ midas-author/src/app/startwiz/stepwizard.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { ArchwizardModule } from 'angular-archwizard';
-
 import { WizardModule } from 'oarng';
 import { StepWizardComponent } from './stepwizard.component';
 import { RouterModule } from '@angular/router';
@@ -15,7 +13,6 @@ import { ComponentsModule } from './components/components.module';
         BrowserAnimationsModule,
         ReactiveFormsModule,
         FormsModule,
-        ArchwizardModule,
         WizardModule,
         ComponentsModule,
         RouterModule
